0e35545a2efa3769bfd10bf6035f5683b84baf9a
ArthurMaciel95 <arthurnmrocha@gmail.com>
2023-10-06 10:58:15 -0300

    feat() integrando api

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,21 +4,131 @@ import { useRouter } from 'next/router'
 
 import { Icon } from '@iconify/react'
 import toast from 'react-hot-toast'
-
+import GoogleMapReact, { Position } from 'google-map-react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import { Ratting } from '../../components/Ratting'
 import BottomNavigation from '../../components/Partials/BottomNavigation'
 import GoogleMaps from '../../components/GoogleMaps'
 import Link from 'next/link'
+import { useFetch } from '../../services/useFetch'
+import React, { useEffect } from 'react'
+import styleGoogleMaps from '../../styles/googleMapsStyle/main'
+import { errorHandler } from '../../services/errorHandler'
 
-interface FormProps {
-  email: string
-  password: string
+import { api } from '../../services/axios'
+import { set } from 'react-hook-form'
+interface IPlaces {
+  results: {
+    uuid: string
+    nome: string
+    lat: number
+    lon: number
+    categoria: string
+    icone: string
+  }[]
 }
-
 const Home: NextPage = () => {
+  const [currentPosition, setCurrentPosition] = React.useState({
+    latitude: 0,
+    longitude: 0,
+  })
+  const [places, setPlaces] = React.useState<IPlaces>()
   const router = useRouter()
+
+  /* const { data } = useFetch<IPlaces>(
+    '/pontos-turisticos?lat=-1.459549&lon=-48.49707&raio=14.28&categorias=10000,13000,14000,16000,18000,19000'
+  )
+ */
+  function locationError(error: any) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        return 'Usuário negou a solicitação de geolocalização.'
+      case error.POSITION_UNAVAILABLE:
+        return ' Local informado indisponível.'
+      case error.TIMEOUT:
+        return 'O pedido de localização do usuário expirou.'
+      case error.UNKNOWN_ERROR:
+        return 'Erro Desconhecido.'
+    }
+  }
+
+  function limitarCaracteres(texto: string, limite: number) {
+    if (texto.length <= limite) {
+      return texto // Retorna a string sem fazer alterações se estiver dentro do limite.
+    } else {
+      const textoLimitado = texto.slice(0, limite) + '...'
+      return textoLimitado // Retorna a string cortada no limite especificado.
+    }
+  }
+
+  async function getPlaces() {
+    try {
+      const response = await api.get(
+        `/pontos-turisticos?lat=${currentPosition.latitude}&lon=${currentPosition.longitude}&raio=14.28&categorias=10000,13000,14000,16000,18000,19000`
+      )
+      setPlaces(response.data)
+      console.log(response)
+    } catch (erro: any) {
+      errorHandler(erro)
+    }
+  }
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      const currentPosition = navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          setCurrentPosition({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+          console.log(position)
+        },
+        (error: GeolocationPositionError) => {
+          console.log(locationError(error))
+          toast.error(locationError(error) as string, {
+            duration: 5000,
+          })
+        }
+      )
+    }
+    getPlaces()
+    console.log(places)
+  }, [])
+
+  const CurrentLocationMarker = ({ text }: { text: string }) => (
+    <Icon
+      icon="ic:round-emoji-people"
+      className="-translate-x-2 -translate-y-2"
+      fontSize={32}
+      color="red"
+    />
+  )
+  const handleApiLoaded = (map: any, maps: any) => {
+    // use map and maps objects
+    map.setOptions({
+      disableDefaultUI: true,
+      zoomControl: true,
+      center: {
+        lat: currentPosition.latitude,
+        lng: currentPosition.longitude,
+      },
+      zoom: 11,
+    })
+    maps.event.addListener(map, 'click', function (e: any) {
+      console.log(e.latLng.lat())
+      console.log(e.latLng.lng())
+    })
+  }
+
+  const defaultProps = {
+    center: {
+      lat: currentPosition.latitude,
+      lng: currentPosition.longitude,
+    },
+    zoom: 11,
+    styles: styleGoogleMaps,
+  }
 
   return (
     <main className="relative ">
@@ -35,98 +145,85 @@ const Home: NextPage = () => {
         </nav>
       </header>
       <section className="mx-4 my-4">
-        <h1 className="text-3xl font-bold text-brand-gray-900">
-          Bem vindo, Angelo!
+        <h1 className="text-2xl font-bold text-brand-gray-900">
+          Seja Bem vindo
         </h1>
-        <p className="text-brand-gray-500 text-2xl mt-1">Explorar</p>
-        <div>
-          {/* <h2 className="text-2xl font-bold text-brand-gray-900 mt-10">
-            O que você deseja fazer?
-          </h2> */}
-          <div className="mt-3">
-            <Swiper slidesPerView={3} spaceBetween={12} className="">
-              <SwiperSlide className="flex flex-col">
-                <Link href="/dashboard/place">
-                  <img
-                    src="/img/turismo1.png"
-                    alt=""
-                    className="aspect-square rounded-3xl drop-shadow-lg"
-                  />
-                  <div className="flex  flex-col mt-1">
-                    <div className="flex">
-                      <Ratting />
+        <p className="text-brand-gray-500 text-2xl mt-1">Lugares por perto</p>
+
+        <div className="mt-3">
+          <Swiper slidesPerView={3} spaceBetween={12} className="">
+            {places?.results?.map((place) => {
+              return (
+                <SwiperSlide key={place.uuid} className="flex flex-col">
+                  <Link href={`/dashboard/place/${place.uuid}`}>
+                    <img
+                      src={place.icone}
+                      alt=""
+                      className="aspect-square rounded-3xl drop-shadow-lg bg-brand-green-300 w-full"
+                    />
+                    <div className="flex  flex-col mt-1">
+                      {/* <div className="flex">
+                          <Ratting />
+                        </div> */}
+                      <h3 className="text-base">
+                        {limitarCaracteres(place.nome, 20)}
+                      </h3>
+                      <div className=" flex items-center">
+                        <span className="text-sm text-brand-gray-500">
+                          {place.categoria}
+                        </span>
+                      </div>
                     </div>
-                    <h3 className="text-base">Cristo Redentor</h3>
-                    <div className=" flex items-center">
-                      <span className="text-sm text-brand-gray-500">
-                        Rio de Janeiro, RJ
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              </SwiperSlide>
-              <SwiperSlide className="flex flex-col">
-                <Link href="/dashboard/place">
-                  <img
-                    src="/img/turismo2.png"
-                    alt=""
-                    className="aspect-square rounded-3xl  drop-shadow-lg"
-                  />
-                  <div className="flex  flex-col mt-2">
-                    <div className="flex">
-                      <Ratting />
-                    </div>
-                    <h3 className="text-base">Teatro da Paz</h3>
-                    <div className=" flex items-center">
-                      <span className="text-sm text-brand-gray-500">
-                        Belém, PA
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              </SwiperSlide>
-              <SwiperSlide className="flex flex-col">
-                <Link href="/dashboard/place">
-                  <img
-                    src="/img/turismo1.png"
-                    alt=""
-                    className="aspect-square rounded-3xl drop-shadow-lg"
-                  />
-                  <div className="flex  flex-col mt-2">
-                    <div className="flex">
-                      <Ratting />
-                    </div>
-                    <h3 className="text-base">Cristo Redentor</h3>
-                    <div className=" flex items-center">
-                      <span className="text-sm text-brand-gray-500">
-                        Rio de Janeiro, RJ
-                      </span>
-                    </div>
-                  </div>
-                </Link>
-              </SwiperSlide>
-            </Swiper>
-            {/* <button className="flex justify-between items-center w-full bg-brand-yellow-300 p-3 rounded-lg">
-              <span className="text-brand-gray-900 font-bold text-2xl">
-                Criar um novo lugar
-              </span>
-              <Icon
-                icon="ant-design:plus-outlined"
-                color="#528fa7"
-                fontSize={44}
-              />
-            </button>
-            <button className="flex justify-between items-center w-full bg-brand-yellow-300 p-3 rounded-lg mt-5">
-              <span className="text-brand-gray-900 font-bold text-2xl">
-                Explorar novos lugares
-              </span>
-              <Icon icon="bx:bxs-search" color="#528fa7" fontSize={44} />
-            </button> */}
-          </div>
+                  </Link>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
         </div>
       </section>
-      <section className=" mb-[73px]">
-        <GoogleMaps />
+      <section className="mb-[72px]">
+        {typeof navigator !== 'undefined' && navigator?.geolocation ? (
+          <div className="aspect-square rounded-lg" style={{ width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{
+                key: 'AIzaSyAXVy2ejGB5cOb_FPd0J2mhxaMjJ4It6JA',
+              }}
+              yesIWantToUseGoogleMapApiInternals
+              onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+              defaultCenter={defaultProps.center}
+              defaultZoom={defaultProps.zoom}
+              options={{
+                styles: defaultProps.styles,
+              }}
+            >
+              <CurrentLocationMarker
+                lat={currentPosition.latitude}
+                lng={currentPosition.longitude}
+              />
+              {places?.results?.map((place) => {
+                return (
+                  <div
+                    key={place.uuid}
+                    lat={place.lat}
+                    lng={place.lon}
+                    className="flex flex-col justify-center items-center"
+                  >
+                    <img
+                      src={place.icone}
+                      alt=""
+                      className="aspect-square rounded-lg bg-brand-green-300 w-10"
+                    />
+                    <p className="text-xs text-white">{place.nome}</p>
+                  </div>
+                )
+              })}
+            </GoogleMapReact>
+          </div>
+        ) : (
+          <div className="aspect-square bg-brand-blue-100 flex justify-center items-center">
+            <p>Por Favor, Ative a geolocalização...</p>
+          </div>
+        )}
       </section>
 
       <BottomNavigation />
