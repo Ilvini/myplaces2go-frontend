7c1cf83d1ced0cc75c0c6c8f3458141d8c695db1
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-09 22:53:12 -0300

    Add client-side rendering and update Google Maps integration.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
+'use client'
+
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 
@@ -71,9 +73,11 @@ const Home: NextPage = () => {
       setLoading(false)
       errorHandler(erro)
     }
+    console.log("busca realizada");
+    
   }
 
-  async function getInfoAboutLatAndLong() {
+  const getInfoAboutLatAndLong = useCallback(async () => {
     try {
       const response = await fetch(
         `https://nominatim.openstreetmap.org/reverse?lat=${currentPosition.latitude}&lon=${currentPosition.longitude}&format=json`,
@@ -98,6 +102,45 @@ const Home: NextPage = () => {
     } catch (error) {
       console.log(error)
     }
+  }, [currentPosition])
+
+  const defaultProps = {
+    center: {
+      lat: currentPosition.latitude,
+      lng: currentPosition.longitude,
+    },
+    zoom: 14.28,
+    styles: styleGoogleMaps,
+  }
+
+  const CurrentLocationMarker = ({ text }: { text: string }) => (
+    <Icon
+      icon="ic:round-emoji-people"
+      className="-translate-x-2 -translate-y-2 z-50 bg-white rounded-full p-1 shadow-md"
+      fontSize={32}
+      color="red"
+    />
+  )
+  const handleApiLoaded = (map: any, maps: any) => {
+    // use map and maps objects
+    map.setOptions({
+      disableDefaultUI: true,
+      zoomControl: true,
+      center: {
+        lat: currentPosition.latitude,
+        lng: currentPosition.longitude,
+      },
+      zoom: defaultProps.zoom,
+    })
+
+    /*  maps.event.addListener(map, 'click', function (e: any) {
+      console.log(e.latLng.lat())
+      console.log(e.latLng.lng())
+    }) */
+  }
+
+  const handleUpdateMap = (lat: number, long: number, e: any) => {
+    getPlaces(lat, long, e.zoom)
   }
 
   useEffect(() => {
@@ -157,10 +200,10 @@ const Home: NextPage = () => {
           (position: GeolocationPosition) => {
             //tentar novamente o swr com essa alteração abaixo
             getPlaces(position.coords.latitude, position.coords.longitude)
-            setCurrentPosition({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            })
+            // setCurrentPosition({
+            //   latitude: position.coords.latitude,
+            //   longitude: position.coords.longitude,
+            // })
           },
           (error: GeolocationPositionError) => {
             console.log(locationError(error))
@@ -175,49 +218,10 @@ const Home: NextPage = () => {
     return () => clearTimeout(timer)
   }, [currentPosition])
 
-  const defaultProps = {
-    center: {
-      lat: currentPosition.latitude,
-      lng: currentPosition.longitude,
-    },
-    zoom: 14.28,
-    styles: styleGoogleMaps,
-  }
-
-  const CurrentLocationMarker = ({ text }: { text: string }) => (
-    <Icon
-      icon="ic:round-emoji-people"
-      className="-translate-x-2 -translate-y-2 z-50 bg-white rounded-full p-1 shadow-md"
-      fontSize={32}
-      color="red"
-    />
-  )
-  const handleApiLoaded = (map: any, maps: any) => {
-    // use map and maps objects
-    map.setOptions({
-      disableDefaultUI: true,
-      zoomControl: true,
-      center: {
-        lat: currentPosition.latitude,
-        lng: currentPosition.longitude,
-      },
-      zoom: defaultProps.zoom,
-    })
-
-    /*  maps.event.addListener(map, 'click', function (e: any) {
-      console.log(e.latLng.lat())
-      console.log(e.latLng.lng())
-    }) */
-  }
-
-  const handleUpdateMap = (lat: number, long: number, e: any) => {
-    getPlaces(lat, long, e.zoom)
-  }
-
   return (
-    <main className="relative">
+    <div className="relative">
       <HeaderNavigation backRoute="" />
-      <section className="mx-4 my-4">
+      <section className="mx-4 my-4 bg-white">
         {/*   <h1 className="text-2xl font-bold text-brand-gray-900">
           Seja Bem vindo
         </h1> */}
@@ -274,7 +278,7 @@ const Home: NextPage = () => {
           >
             {places?.results.length ? (
               <>
-                <div className="absolute left-2 top-3 flex flex-wrap z-30 space-x-2">
+                <div className="absolute left-2 top-3 flex flex-wrap z-30 space-x-2 ">
                   <Link href="/info-city">
                     <button className="text-sm drop-shadow-lg bg-white flex items-center px-4 py-2 text-brand-gray-900 font-normal  rounded-full">
                       <Icon icon="mdi:city" className="mr-2" /> Rio de Janeiro
@@ -296,6 +300,13 @@ const Home: NextPage = () => {
                 </div>
 
                 <GoogleMapReact
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                  }}
                   onClick={() => {
                     if (openWindow) setOpenWindow(null)
                   }}
@@ -409,9 +420,8 @@ const Home: NextPage = () => {
       </section>
 
       <BottomNavigation />
-    </main>
+    </div>
   )
 }
 
 export default Home
-
