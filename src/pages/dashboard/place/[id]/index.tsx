eb5c0d9b538b1e54c665cc39b51af45da753d13b
Arthur Rocha <62079236+ArthurMaciel95@users.noreply.github.com>
2023-12-07 09:35:10 -0300

    fix() removendo caixa de dialogo google maps

--- src/pages/dashboard/place/[id]/index.tsx
+++ src/pages/dashboard/place/[id]/index.tsx
@@ -99,14 +99,6 @@ const PlaceDetails: NextPage = ({ data }) => {
               height: 627,
               alt: data?.results.nome,
             },
-            {
-              url: data?.results.imagens[1] || '/seo.png',
-              width: 1200,
-              height: 627,
-              alt: data?.results.nome,
-            },
-            { url: data?.results.imagens[2] || '/seo.png' },
-            { url: data?.results.imagens[3] || '/seo.png' },
           ],
           site_name: 'My Place 2 Go',
         }}
@@ -291,4 +283,3 @@ export const getServerSideProps = async (context: any) => {
     },
   }
 }
-
